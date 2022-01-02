90266dfdcd45cc697e70249a911063c1cf449ada
Adam <adam1309.am@gmail.com>
2022-01-02 18:28:35 +0100

    parametized tests

--- src/test/java/machine/DrinkMaker.java
+++ src/test/java/machine/DrinkMaker.java
@@ -12,20 +12,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Drink maker should")
 class DrinkMakerTest {
+    public static final String COFFEE = "Coffee";
+    public static final String TEA = "Tea";
+    public static final String CHOCOLATE = "Chocolate";
+    public static final String ORANGE = "Orange";
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+    public static final int TWO = 2;
     private final DrinkMaker drinkMaker = new DrinkMaker();
 
     private static Stream<Arguments> givenArguments() {
         return Stream.of(
-                Arguments.of("Coffee", 2, 0.6, "C:2:0", false),
-                Arguments.of("Tea", 1, 0.4, "T:1:0", false),
-                Arguments.of("Chocolate", 0, 0.5, "H::", false),
-                Arguments.of("Coffee", 0, 0.2, "M:Not enough money given", false),
-                Arguments.of("Tea", 0, 0.2, "M:Not enough money given", false),
-                Arguments.of("Chocolate", 0, 0.2, "M:Not enough money given", false),
-                Arguments.of("Orange", 0, 0.6, "O::", false),
-                Arguments.of("Coffee", 1, 0.6, "Ch:1:0", true),
-                Arguments.of("Tea", 1, 0.6, "Th:1:0", true),
-                Arguments.of("Chocolate", 1, 0.6, "Hh:1:0", true)
+                Arguments.of(COFFEE, TWO, 0.6, "C:2:0", false),
+                Arguments.of(TEA, ONE, 0.4, "T:1:0", false),
+                Arguments.of(CHOCOLATE, ZERO, 0.5, "H::", false),
+                Arguments.of(COFFEE, ZERO, 0.2, "M:Not enough money given", false),
+                Arguments.of(TEA, ZERO, 0.2, "M:Not enough money given", false),
+                Arguments.of(CHOCOLATE, ZERO, 0.2, "M:Not enough money given", false),
+                Arguments.of(ORANGE, ZERO, 0.6, "O::", false),
+                Arguments.of(COFFEE, ONE, 0.6, "Ch:1:0", true),
+                Arguments.of(TEA, ONE, 0.6, "Th:1:0", true),
+                Arguments.of(CHOCOLATE, ONE, 0.6, "Hh:1:0", true)
         );
     }
 
